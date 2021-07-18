1f2fb30337d2152d4001517db87255c69877f31d
Igor <basprogrod@bk.ru>
2021-07-18 11:01:16 +0300

    add rows

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-'use strict'
-
 const canvas = document.getElementById('canvas')
 
 const ctx = canvas.getContext('2d')
-const WIDTH = 300
-const HEIGHT = 500
+const WIDTH = 500
+const HEIGHT = 700
+const CELL_COLOR_1 = '#7762ac'
+const CELL_COLOR_2 = '#aa6da8'
+const QUANTIFICATION = 4
+const ROW_MARGIN = 7
+const ROWS_QUONTITY = 12
 
 canvas.width = WIDTH
 canvas.height = HEIGHT
@@ -19,8 +22,8 @@ let raf = undefined
 
 class Item {
   constructor(x, y) {
-    this.width = 50
-    this.height = 80
+    this.width = 25
+    this.height = 40
     this.x = x
     this.y = y
   }
@@ -38,7 +41,7 @@ function getItems(Item, quontity, widthOfCanvas) {
 }
 
 function removeAll() {
-  ctx.fillStyle = "#aaa"
+  ctx.fillStyle = "#7b4572"
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
 }
 
@@ -55,37 +58,43 @@ function drowCell(x, y, w, h) {
 }
 
 function drowItems(offset = 0) {
-  ctx.beginPath()
-
-  items.forEach((item, index) => {
-    ctx.fillStyle = `${!index ? '#00ff00' : '#fff'}`
-    
-    let x = item.x + offset
-
-    const loop = Math.abs(Math.floor((item.x + offset) / WIDTH))
-    // console.log("-> loop", loop)
-
-    if (offset < 0) {
-      x = item.x + offset > 0 ? 
-      item.x + offset
-      :
-      ctx.fillRect(item.x + offset + WIDTH, item.y + 50, item.width, item.height)
-      ctx.font = "italic 20px Arial";
-      ctx.fillText('kek', item.x + offset + WIDTH, 20);
-    } else {
-      x = item.x
-      globalOffset = 0
-      xLastPosition = 0
-    } 
-    
-    ctx.fillRect(item.x + offset, item.y + 50, item.width, item.height)
-    // ctx.fillRect(x + WIDTH, item.y + 50, item.width, item.height)
-    ctx.fillStyle = "#000";
-    // drowCell(x, item.y + 20, item.width + x + offset, item.height)
-    ctx.font = "italic 20px Arial";
-    
-    ctx.fillText(loop * items.length + index, x, 20);
-  })
+  ctx.font = "italic 14px Arial";
+  let flag = true
+  
+  for (let i = 0; i < ROWS_QUONTITY; i++) {
+    items.forEach((item, index) => {
+      const loop = Math.abs(Math.floor((item.x + offset) / WIDTH))
+      const loopForDigits = Math.abs(Math.floor((item.x + globalOffset) / WIDTH))
+  
+      if (!(index % QUANTIFICATION)) flag = !flag    
+  
+      ctx.fillStyle = `${flag ? CELL_COLOR_1 : CELL_COLOR_2}`
+  
+      let x = item.x + offset + WIDTH * loop
+      let y = item.y + i * (item.height + ROW_MARGIN)
+  
+      if (globalOffset < 0) {
+  
+        if (item.x + offset < 0) {
+          ctx.fillRect(item.x + offset, y, item.width, item.height)
+        }
+        
+      } else {
+        x = item.x
+        globalOffset = 0
+        xLastPosition = 0
+      } 
+      
+      ctx.fillRect(x, y, item.width, item.height)
+      ctx.fillStyle = "#FFFc";
+  
+      ctx.fillText(loopForDigits * items.length + index, x + 5, 20);
+      
+    })
+  }
+  
+  
+  ctx.fillText('offset: ' + offset , 20, HEIGHT - 20);
 }
 
 function mouseMove(e) {
@@ -97,12 +106,12 @@ function mouseMove(e) {
 
 function draw() {
   removeAll()
-  drowItems(globalOffset)
+  drowItems(globalOffset % WIDTH)
   raf = requestAnimationFrame(draw)
-  ctx.fillText('offset: ' + globalOffset , 100, 200);
+  ctx.fillText('GLobal offset: ' + globalOffset , 20  , HEIGHT - 40);
 }
 
-const items = getItems(Item, 3, WIDTH)
+const items = getItems(Item, 16, WIDTH)
 console.log("-> items", items)
 
 removeAll()
@@ -117,6 +126,7 @@ canvas.addEventListener('mousedown', (e) => {
 })
 
 canvas.addEventListener('mouseup', (e) => {
+  
   xLastPosition = globalOffset
   cancelAnimationFrame(raf)
   canvas.removeEventListener('mousemove', mouseMove)
@@ -124,25 +134,4 @@ canvas.addEventListener('mouseup', (e) => {
 
 
 
-// ctx.beginPath()
 
-// ctx.moveTo(10, 10)
-// ctx.lineTo(50, 10)
-// ctx.lineTo(50, 80)
-// ctx.lineTo(10, 80)
-// ctx.closePath()
-
-// ctx.moveTo(60, 10)
-// ctx.lineTo(100, 10)
-// ctx.lineTo(100, 80)
-// ctx.lineTo(60, 80)
-// ctx.closePath()
-
-
-
-
-
-
-// ctx.stroke()
-// ctx.fill()
-// ctx.moveTo(x + 20, y)
